6c61e19087b3a25ecf045f119d36b8a6fa956d4b
AleksDavitkov <aleksdavitkov@hotmail.com>
2025-10-14 23:12:09 +0200

    dev 1

---
+++ lynk/src/app/features/category/category-list/category-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.scss']
+})
+export class CategoryListComponent implements OnInit {
+
+  constructor(private categoryService: CategoryService) {}
+  categories$?: Observable<Category[]>;
+
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
+  }
+}
